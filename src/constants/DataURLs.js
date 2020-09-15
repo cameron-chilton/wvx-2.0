ba99fcc8ea3e4fa0adca8823ca8e1986a836ecad
Cameron Chilton <cameron.chilton@ablevets.com>
2020-09-15 17:05:12 -0400

    update

--- src/constants/DataURLs.js
+++ src/constants/DataURLs.js
@@ -2,8 +2,8 @@
 export const GET_VOICE_COUNT = 'https://www.whovox.com/php/getVoiceCount.php';
 
 // URL for voice of page
-export const VOICE_OF_URL = 'https://www.whovox.com/php/voiceOf.php';
+export const VOICE_OF_URL = 'https://www.whovox.com/public/voiceOf.php';
 
 // URL for Privacy Policy Page
-export const PRIVACY_POLICY_URL = 'https://www.whovox.com/php/voiceOf.php';
+export const PRIVACY_POLICY_URL = 'https://www.whovox.com/public/privacy-policy.html';
 
